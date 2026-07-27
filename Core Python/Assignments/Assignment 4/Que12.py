adc8dad959fb8b2938dd9280f2baec3fae15a5e5
Kiran_07 <kiranjadhav23388@gmail.com>
2026-07-27 22:07:12 +0530

    Add files via upload
    Assignment 4

---
+++ Core Python/Assignments/Assignment 4/Que12.py
@@ -0,0 +1,14 @@
+no=int(input("Enter the number you want to check= "))
+count=len(str(no))
+temp=no
+total=0
+while(no>0):
+    d=no%10
+    total=total+(d**count)
+    no=no//10
+print(total)
+if total==temp:
+    print("The number is Armstrong")
+else:
+    print("The number is not Armstrong")
+
---
+++ Core Python/Assignments/Assignment 4/Que4.py
@@ -0,0 +1,7 @@
+#QUE4...WAP to print factorial of a number
+
+n=int(input("Enter number:"))
+fact=1
+for i in range (1,n+1):
+    fact*=i
+print("Factorial=",fact)
