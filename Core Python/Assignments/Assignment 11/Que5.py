b92ec06d183844c4d3d199a124468b8aeec63280
Kiran_07 <kiranjadhav23388@gmail.com>
2026-08-16 23:29:33 +0530

    Delete Core Python/Assignments/Assignment 11/Que5.py

--- Core Python/Assignments/Assignment 11/Que5.py
+++
@@ -1,8 +0,0 @@
-# Que5...python program to sort a list according to the length of elemnts  within the list..
-
-
-li = ["swara", "panu", "Ishuuu", "sai", "om"]
-print("Original list=",li)
-li.sort(key=len)
-
-print("Sorted List =", li)
